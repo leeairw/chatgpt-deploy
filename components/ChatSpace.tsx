32f0324868b8af9ea8dd662c51aeecbe3ff9cfbf
Shen Chen <shenseanchen@Shens-MacBook-Pro.local>
2023-03-09 18:08:04 -0500

    make the sample questions clickable

--- components/ChatSpace.tsx
+++ components/ChatSpace.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import { ArrowDownCircleIcon } from '@heroicons/react/24/outline';
-import { collection, orderBy, query } from 'firebase/firestore';
+import { collection, orderBy, query,serverTimestamp, addDoc  } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
 import React from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { db } from '../firebase';
 import PastMessage from './PastMessage';
 import { AcademicCapIcon, DeviceTabletIcon, ShieldExclamationIcon} from '@heroicons/react/24/outline'
+import { toast } from 'react-hot-toast';
+import useSWR from "swr";
 
 
 type Props = {
@@ -26,6 +28,59 @@ function ChatSpace({chatId}: Props) {
       orderBy("createdAt", "asc")
     )
   );
+
+  // sample questions
+  const sample_questions = [
+    "What's the difference b/w 'Ser' and 'Está' in Spanish?",
+    "How to say I love you in French?",
+    "Make up a fun dialogue for kids to learn Japanese.",
+    "Summarize text or explain a concept.",
+    "Create content in all languages."
+  ]
+
+  // set the model for sample question
+  const { data: model, mutate: setModel } = useSWR('model', {
+    fallbackData: 'text-davinci-003'
+  });
+
+  const sendSampleQuestion = async(sample_question: string) => {
+    // Define the sample question input format
+    const sample_question_fb: Message = {
+      text: sample_question,
+      createdAt: serverTimestamp(),
+      user: {
+          _id: session?.user?.email!,
+          name: session?.user?.name!,
+          type: "UserRequest",
+          avatar: session?.user?.image! || `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      }
+    }
+
+    await addDoc(
+        collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+        sample_question_fb 
+    )
+
+    console.log("Current sample question: ", sample_question)
+
+    // Toast notification to say Loading
+    const notification = toast.loading('Smart Lingo is thinking...')
+    await fetch('/api/askQuestions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify({
+            prompt: sample_question_fb, chatId, model, session
+        }),
+    }).then(() => {
+        // Toast notificaion to say successful!
+        toast.success("Smart Lingo has responded!", {
+            id: notification,
+        })
+    })
+  };
+
   
   // console.log('ChatSpace Messages: ', messages)
   // console.log('ChatSpace Message Mapped: ', messages?.docs.map(message => (message.id)))
@@ -54,30 +109,27 @@ function ChatSpace({chatId}: Props) {
                         <AcademicCapIcon className="h-8 w-8 text-purple-400"/><h2>Examples</h2>
                     </div>
                 </div>
-                <div className='space-y-2'>
-                    <p className='infoText'>"What's the difference b/w 'Ser' and 'Está' in Spanish?"</p>
-                    <p className='infoText'>"How to say I love you in French?"</p>
-                    <p className='infoText'>"Make up a fun dialogue for kids to learn Japanese."</p>
+                <div className='space-y-2 '>
+                   {sample_questions.map((sample_q) => {
+                    console.log("Current sample_q: ", sample_q)
+                    const handleClick = async () => {
+                      await sendSampleQuestion(sample_q);
+                    };
+                    
+                    return (
+                      <div onClick={handleClick} className={`sampleQuestionButton`}>
+                        <p className=''>{sample_q}</p>
+                      </div>
+                    )            
+                  }
+                    
+                   )}
                 </div>   
             </div>
             
 
-            {/* Capabilities Icon */}
-            <div className='flex flex-col items-center justify-center'>
-                <div className='flex flex-col items-center justify-center mb-2'>
-                    <div className='flex flex-row space-x-2 items-center'>
-                        <DeviceTabletIcon className="h-8 w-8 text-purple-400"/><h2>Examples</h2>
-                    </div>
-                </div>
-                <div className='space-y-2'>
-                    <p className='infoText'>"Summarize text or explain a concept."</p>
-                    <p className='infoText'>"Answer questions."</p>
-                    <p className='infoText'>"Create content in all languages."</p>
-                </div>   
-            </div>
-
             {/* Limitations Icon */}
-            <div className='flex flex-col items-center justify-center'>
+            {/* <div className='flex flex-col items-center justify-center'>
                 <div className='flex flex-col items-center justify-center mb-2'>
                     <div className='flex flex-row space-x-2 items-center'>
                         <ShieldExclamationIcon className="h-8 w-8 text-purple-400"/><h2>Limitations</h2>
@@ -87,8 +139,8 @@ function ChatSpace({chatId}: Props) {
                     <p className='infoText'>"Cannot answer certain racial and political questions."</p>
                     <p className='infoText'>"Cannot generate images."</p>
                     <p className='infoText'>"Cannot answer anything happened after 2021."</p>
-                </div>   
-            </div>
+                </div>    */}
+            {/* </div> */}
           </div>  
         </div>
         )}
--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -5,9 +5,21 @@ const queryGPT = async (prompt: string, chatId:string, model: string) => {
     // can use chatId to use contextual chat
 
     // set up params for openai responses
+    // model === "gpt-3.5-turbo" || model === "gpt-3.5-turbo-0301" ? (
+    // ) : (
+    // )
+    // const res = await openai.createChatCompletion({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [{role: "user", content: "Hello world"}],
+    // })
+    // .then(res => res.data.choices[0].text)
+    // .catch(
+    //     (err) => 
+    //     `Smart Lingo was unable to find an answer for that! (Error: ${err.message})`
+    // );
     const res = await openai.createCompletion({
-        model,
-        prompt,
+        model: model,
+        prompt: prompt,
         temperature: 0.2, // wanna be more creative (0.9) or more logical (0.1)
         max_tokens: 1000,
         top_p: 1,
